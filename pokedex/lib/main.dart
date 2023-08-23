b2d3d04c1f1091c4a6c19bad42985ada1eba4791
SifAa <sif_aagaard_svendsen@hotmail.com>
2023-08-23 08:22:06 +0200

    Initial build
     with main.dart containing defined first screens and routes

---
+++ pokedex/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/screens/pokedex.dart';
+import 'package:pokedex/screens/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 254, 0, 2)),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/pokedex': (context) => const Pokedex(),
+      },
+    );
+  }
+}
