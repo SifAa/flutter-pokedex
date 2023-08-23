9522e0fd257c17eb25e75f3da950c76826a852c4
SifAa <sif_aagaard_svendsen@hotmail.com>
2023-08-23 08:23:36 +0200

    Create pokedex.dart

---
+++ pokedex/lib/screens/pokedex.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pokedex extends StatefulWidget {
+  const Pokedex({super.key});
+
+  @override
+  State<Pokedex> createState() => _PokedexState();
+}
+
+class _PokedexState extends State<Pokedex> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
